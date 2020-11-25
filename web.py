bcc2f7101cf5e75313712f116ca9a3e580a5c7d0
Nceba Dumasi <ndumasi@c5r8s5.co.za>
2020-11-25 18:41:27 +0200

    things completed

---
+++ web.py
@@ -0,0 +1,50 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import requests
+import os
+import sys
+# import time
+
+
+def clear():
+    os.system('clear')
+
+
+driver = ''
+i = ['q']
+clear()
+print("Sort searches: \n\n - h - For highest to lowest\n - l - For lowest to highest")
+filter_ = input(' > ').lower()
+
+def init():
+    global driver
+    PATH = f"{os.environ['HOME']}/Web_Crawler/.chromedriver"
+
+    while len(i[-1]) > 0:
+        i.append(input('What item are we looking for: '))
+    
+    driver = webdriver.Chrome(PATH)
+
+    driver.get('https://www.olx.co.za')
+
+def sort_olx(filter_):
+    if filter_ == 'l':
+        sort_by_lowest = driver.find_element_by_xpath('//*[@id="container"]/main/div/section/div/div/div[4]/div[2]/div/div[1]/div/div[2]/ul/li[3]/div')
+        sort_by_lowest.click()
+    elif filter_ == 'h':
+        sort_by_highest = driver.find_element_by_xpath('//*[@id="container"]/main/div/section/div/div/div[4]/div[2]/div/div[1]/div/div[2]/ul/li[4]/div')
+        sort_by_highest.click()
+def main():
+    init()
+    driver.implicitly_wait(6)
+    search_bar = driver.find_element_by_xpath('//*[@id="container"]/header/div/div/div[2]/div/div/div[2]/div/form/fieldset/div/input')
+    search_btn = driver.find_element_by_xpath('//*[@id="container"]/header/div/div/div[2]/div/div/div[3]')
+    search_bar.send_keys(i[1], Keys.ENTER)
+
+    sort_by = driver.find_element_by_xpath('//*[@id="container"]/main/div/section/div/div/div[4]/div[2]/div/div[1]/div/div[2]/div')
+    sort_by.click()
+    sort_olx(filter_)
+
+
+
+main()
